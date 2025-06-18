c1c9a9c6fe268f14fa442af2d70aabb8a854871f
fajar hermawan <vjayhermawan@gamil.com>
2025-06-18 16:02:32 +0700

    finally

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import "./styles/global.css"
 import Page from './routes/index.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Page />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
